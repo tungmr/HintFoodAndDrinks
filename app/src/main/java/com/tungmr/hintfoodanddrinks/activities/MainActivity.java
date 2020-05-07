6de6867afe11a40aa0ed8f80c1dc91a81cc7413f
tungmr <tungars23697@gmail.com>
2020-05-07 13:13:31 +0700

    sign in, sign up gui. hidden app name

--- app/src/main/java/com/tungmr/hintfoodanddrinks/activities/MainActivity.java
+++ app/src/main/java/com/tungmr/hintfoodanddrinks/activities/MainActivity.java
@@ -1,14 +1,16 @@
-package com.tungmr.hintfoodanddrinks;
+package com.tungmr.hintfoodanddrinks.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.tungmr.hintfoodanddrinks.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
     }
 }
